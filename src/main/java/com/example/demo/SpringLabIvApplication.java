652b9066c7579b96cb0a8b6a60873c4f4bc8cc64
andremeneses0103 <andremeneses0103@gmail.com>
2024-08-14 19:25:36 -0300

    Adicionando os arquivos iniciais

---
+++ src/main/java/com/example/demo/SpringLabIvApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabIvApplication.class, args);
+	}
+
+}
